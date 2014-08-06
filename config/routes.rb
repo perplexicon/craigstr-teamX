edaab069462533837ad76371055075f96dbf5b22
Nicholas Narh <nicknarh@gmail.com>
2014-08-06 10:30:41 -0400

    Add Geographic Regions
    * Create region Model and Migrations
    * Create region Controller and Views
    * Update routes to include region
    
    https://trello.com/c/djVjZWj6

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ require "monban/constraints/signed_in"
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :index]
+  resources :regions, only: [:new, :create, :show]
 
-  constraints Monban::Constraints::SignedIn.new do
-    root "regions#index"
-  end
+  root "regions#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,12 @@ ActiveRecord::Schema.define(version: 20140805184357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "regions", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                           null: false
     t.string   "password_digest",                 null: false
