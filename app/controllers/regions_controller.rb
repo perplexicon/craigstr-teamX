0e3c042c4d50c3529c22f20941b8809818b1cb82
Nicholas Narh <nicknarh@gmail.com>
2014-08-06 15:39:05 -0400

    Category Creation
    * create category controller
    * add category routes
    * update regions controller
    * update regions views

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -6,5 +6,9 @@ class RegionsController < ApplicationController
   def new
     @region = Region.new
   end
+
+  def show
+    @region = Region.find(params[:id])
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :index]
   resources :regions, only: [:new, :create, :show]
+  resources :categories, only: [:new, :create, :show]
 
   root "regions#index"
 end
