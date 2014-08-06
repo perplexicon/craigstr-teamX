9d35d8fe12717142652a9beb54bf4a244e5ba3da
Wax <jwaxnyc@gmail.com>
2014-08-06 17:47:42 -0400

    Add Nav Bar, Guest Sign In/Up
    *Nav Bar is site wide
    *Guest Sign in and sign up functionality works
    *Create vanity urls for sign-in, sign-up, sign-out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:users).permit(:email, :password)
   end
 end
