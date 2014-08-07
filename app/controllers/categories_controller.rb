8d598c416a5891a670fdedda426a55be1e7244bb
Nicholas Narh <nicknarh@gmail.com>
2014-08-07 17:23:18 -0400

    fix merge conflict

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,28 @@
 class CategoriesController < ApplicationController
   def new 
+    @region = Region.find(params[:region_id])
     @category = Category.new
   end
+  
+  def create
+    region = Region.find(params[:region_id])
+    category = Category.new(category_params)
+    if category.save
+      redirect_to [region, category]
+    else
+      render :new
+    end
+  end
 
-  def show
-    @category = Category.find(params[:id])
-    @region = @category.region
-    @posts = @category.posts
+ def show
+   @category = Category.find(params[:id])
+   @region = @category.region
+ end 
+  private
+  
+  def category_params
+    params.
+      require(:category).
+      permit(:name)
   end
 end
